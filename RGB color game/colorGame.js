1cee66a486622dbae249784178af9f3700bf7b51
XiangruL <xiangrul@buffalo.edu>
2017-11-10 15:31:25 -0500

    add a new mode - midium

--- RGB color game/colorGame.js
+++ RGB color game/colorGame.js
@@ -1,65 +1,76 @@
+var game = {}
 var sqaureNum = 6;
-var colorSet = generateColorSet(sqaureNum);
+var goal;
+var colorSet;
 var squareSet = document.querySelectorAll(".square");
 // set a random color in the colorSet as a goal
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
-var goal = pickAColor();
 var h1 = document.querySelector("h1");
 var resetButton = document.getElementById("reset");
 var modeBtn = document.querySelectorAll(".mode");
 
-// change game mode
-for (var i = 0; i < modeBtn.length; i++) {
-    modeBtn[i].addEventListener("click", function(){
-        // remove class from all mode buttons
-        for (var j = 0; j < modeBtn.length; j++) {
-            modeBtn[j].classList.remove("selected");
-        }
-        // add to the current one
-        this.classList.add("selected");
-        if (this.textContent == "EASY") {
-            sqaureNum = 3;
-        } else {
-            sqaureNum = 6;
-        }
-        resetGame();
-    });
+game.setupModeBtn = function() {
+    // game mode
+    for (var i = 0; i < modeBtn.length; i++) {
+        modeBtn[i].addEventListener("click", function(){
+            // remove class from all mode buttons
+            for (var j = 0; j < modeBtn.length; j++) {
+                modeBtn[j].classList.remove("selected");
+            }
+            // add to the current one
+            this.classList.add("selected");
+            if (this.textContent == "EASY") {
+                sqaureNum = 3;
+            } else if (this.textContent == "HARD") {
+                sqaureNum = 9;
+            } else {
+                sqaureNum = 6;
+            }
+            game.resetGame();
+        });
+    }
 }
 
-// generate new colors when clicking reset
-resetButton.addEventListener("click", resetGame);
-
-// show goal rgb color in the title
-for (var i = 0; i < squareSet.length; i++) {
-    // initialize squares with different color
-    squareSet[i].style.backgroundColor = colorSet[i];
-    // add event listeners
-    squareSet[i].addEventListener("click", function() {
-        var clicked = this.style.backgroundColor;
-        if (clicked == goal) {
-            // change all the other color
-            changeColor(goal);
-            // change h1 background
-            h1.style.backgroundColor = goal;
-            // display the message
-            messageDisplay.textContent = "Congradulations!"
-            resetButton.textContent = "PLAY AGAIN";
-        } else {
-            // set wrong square colot to the body background color
-            this.style.backgroundColor =
-            window.getComputedStyle(document.body, null).getPropertyValue('background-color');
-            // display the message
-            messageDisplay.textContent = "Nah, try again"
-        }
-    })
+game.changeColor = function(color) {
+    // interate all color in the color set
+    for (var i = 0; i < colorSet.length; i++) {
+        squareSet[i].style.backgroundColor = color;
+    }
 }
 
-function resetGame() {
+game.setupSquares = function() {
+    // show goal rgb color in the title
+    for (var i = 0; i < squareSet.length; i++) {
+        // initialize squares with different color
+        squareSet[i].style.backgroundColor = colorSet[i];
+        // add event listeners
+        squareSet[i].addEventListener("click", function() {
+            var clicked = this.style.backgroundColor;
+            if (clicked == goal) {
+                // change all the other color
+                game.changeColor(goal);
+                // change h1 background
+                h1.style.backgroundColor = goal;
+                // display the message
+                messageDisplay.textContent = "Congradulations!"
+                resetButton.textContent = "PLAY AGAIN";
+            } else {
+                // set wrong square colot to the body background color
+                this.style.backgroundColor =
+                window.getComputedStyle(document.body, null).getPropertyValue('background-color');
+                // display the message
+                messageDisplay.textContent = "Nah, try again"
+            }
+        })
+    }
+}
+
+game.resetGame = function() {
     // generate new colors
-    colorSet = generateColorSet(sqaureNum);
+    colorSet = game.generateColorSet(sqaureNum);
     // pick a new goal
-    goal = pickAColor();
+    goal = game.pickAColor();
     // change colors of squares
     for (var i = 0; i < squareSet.length; i++) {
         if (colorSet[i]) {
@@ -77,14 +88,7 @@ function resetGame() {
     resetButton.textContent = "NEW COLORS";
 }
 
-function changeColor(color) {
-    // interate all color in the color set
-    for (var i = 0; i < colorSet.length; i++) {
-        squareSet[i].style.backgroundColor = color;
-    }
-}
-
-function generateColorSet(n) {
+game.generateColorSet = function(n) {
     set = []
     // generate a set with n random number
     for (var j = 0; j < n; j++) {
@@ -104,11 +108,28 @@ function generateColorSet(n) {
     return set;
 }
 
-function pickAColor() {
+game.pickAColor = function() {
     // pick a random color from colorSet
     var random = Math.floor(Math.random() * colorSet.length);
     // display color;
     colorDisplay.textContent = colorSet[random].toUpperCase();
     return colorSet[random];
 }
+
+game.init = function() {
+    // generate new colors when clicking reset
+    resetButton.addEventListener("click", game.resetGame);
+    // set up mode button
+    game.setupModeBtn();
+    // reset and initialize goal and colorSet
+    game.resetGame();
+    // initialize squares
+    game.setupSquares();
+}
+
+// initialize the game
+game.init();
+
+
+
 
