3b4e8607fe90fad12188d13aec17cd39bfdf90d1
XiangruL <xiangrul@buffalo.edu>
2017-11-09 22:11:39 -0500

    RBG color game add a easy mode

--- RGB color game/colorGame.js
+++ RGB color game/colorGame.js
@@ -1,12 +1,71 @@
-var colorSet = generateColorSet(6);
+var sqaureNum = 6;
+var colorSet = generateColorSet(sqaureNum);
 var squareSet = document.querySelectorAll(".square");
 // set a random color in the colorSet as a goal
-var goal = pickAColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var goal = pickAColor();
+var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+// switch between easy and hard mode
+easyBtn.addEventListener("click", function() {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    // genrete new colors
+    sqaureNum = 3;
+    colorSet = generateColorSet(sqaureNum);
+    // pick a new goal
+    goal = pickAColor();
+    for (var i = 0; i < squareSet.length; i++) {
+        // make the extra squares disappear
+        if (i >= colorSet.length) {
+            squareSet[i].style.display = "none";
+        } else {
+            squareSet[i].style.backgroundColor = colorSet[i];
+        }
+    }
+})
+
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    // genrete new colors
+    sqaureNum = 6;
+    colorSet = generateColorSet(sqaureNum);
+    // pick a new goal
+    goal = pickAColor();
+    for (var i = 0; i < squareSet.length; i++) {
+        // make the extra squares disappear
+        squareSet[i].style.backgroundColor = colorSet[i];
+        if (i >= 3) {
+            squareSet[i].style.display = "block";
+        }
+    }
+})
+
+// generate new colors when clicking reset
+resetButton.addEventListener("click", function() {
+    // generate new colors
+    colorSet = generateColorSet(sqaureNum);
+    // pick a new goal
+    goal = pickAColor();
+    // change colors of squares
+    for (var i = 0; i < squareSet.length; i++) {
+        squareSet[i].style.backgroundColor = colorSet[i];
+    }
+    // reset h1 color to background color
+    h1.style.backgroundColor =
+    window.getComputedStyle(document.body, null).getPropertyValue('background-color');
+    // reset messageDisplay disappear
+    messageDisplay.textContent = "";
+    // reset button text from PLAY AGAIN TO NEW COLORS
+    resetButton.textContent = "NEW COLORS";
+})
 
 // show goal rgb color in the title
-colorDisplay.textContent = goal.toUpperCase();
 for (var i = 0; i < squareSet.length; i++) {
     // initialize squares with different color
     squareSet[i].style.backgroundColor = colorSet[i];
@@ -16,8 +75,11 @@ for (var i = 0; i < squareSet.length; i++) {
         if (clicked == goal) {
             // change all the other color
             changeColor(goal);
+            // change h1 background
+            h1.style.backgroundColor = goal;
             // display the message
             messageDisplay.textContent = "Congradulations!"
+            resetButton.textContent = "PLAY AGAIN";
         } else {
             // set wrong square colot to the body background color
             this.style.backgroundColor =
@@ -26,6 +88,13 @@ for (var i = 0; i < squareSet.length; i++) {
             messageDisplay.textContent = "Nah, try again"
         }
     })
+}
+
+function resetGame() {
+    var n = colorSet.length;
+    generateColorSet(n);
+    var newGoal = pickAColor();
+    return newGoal;
 }
 
 function changeColor(color) {
@@ -58,5 +127,8 @@ function generateColorSet(n) {
 function pickAColor() {
     // pick a random color from colorSet
     var random = Math.floor(Math.random() * colorSet.length);
+    // display color;
+    colorDisplay.textContent = colorSet[random].toUpperCase();
     return colorSet[random];
 }
+
